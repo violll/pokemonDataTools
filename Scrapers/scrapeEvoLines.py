0a4d8aa162e45a91f0a1058d5ae3dad0ba1b8b29
violll <gilbarthol3@gmail.com>
2024-05-03 12:11:49 -0400

    feat: wrote getEvoLineSoup method

--- Scrapers/scrapeEvoLines.py
+++ Scrapers/scrapeEvoLines.py
@@ -7,10 +7,19 @@ class scrapeEvoLines():
         # The url from which the evo lines are scraped
         self.url = "https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_evolution_family"
         self.soup = self.getSoup()
+        self.evoLinesSoup = self.getEvoLinesSoup()
 
+    # returns the entire bs4 soup of the website
     def getSoup(self):
         html = requests.get(self.url)
         return BeautifulSoup(html.text, 'html.parser')
+    
+    # returns list of bs4 soup tables of evolution families 
+    # each element in the list corresponds to one region 
+    #  (note: regional variants are grouped with the original line)
+    def getEvoLinesSoup(self):
+        tables = self.soup.findAll("table")
+        return tables
     
 
 if __name__ == "__main__":
