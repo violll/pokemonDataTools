c16877aa420215c1de6b627a2811af53cd284967
violll <gilbarthol3@gmail.com>
2024-05-03 18:28:30 -0400

    refactor: buildTree and buildLine to account for split evolutions

--- Scrapers/scrapeEvoLines.py
+++ Scrapers/scrapeEvoLines.py
@@ -4,8 +4,11 @@ import bigtree
 from bigtree import Node, tree_to_dot, list_to_tree, find
 
 # bs4 filtering criteria
-def notTableRowHeader(tr):
-    return tr.name == "tr" and len(tr.findAll("th")) == 0
+def isEvoLine(tr):
+    return len(tr.findAll("th")) == 0
+
+# def isSplitLine(tr):
+#     return
 
 class scrapeEvoLines():
     def __init__(self) -> None:
@@ -25,51 +28,46 @@ class scrapeEvoLines():
     # each element in the list corresponds to one region 
     #  (note: regional variants are grouped with the original line)
     def getEvoLinesSoup(self):
-        tables = self.soup.findAll("table")
+        tables = self.soup.findAll("table") #[:11]
         return tables
     
     # returns dict of trees for each evolutionary line
     def buildTree(self):
-        """ how to set up a list of evolutionary lines and call/print them
-        alolanR = Node("alolan ratatta")
-        alolanRa = Node("alolan ratticate", parent=alolanR)
-        rattata = Node("rattata")
-        ratticate = Node("ratticate", parent=rattata)
-        l = [alolanR, rattata]
-        for x in l: x.hshow() 
-        """
-
         res = {}
 
         for table in self.evoLinesSoup:
             # gets each family in a row
-            # TODO not sure how this interacts with split lines 
-            rows = [row for row in table.findNext() if notTableRowHeader(row)]
+            # not sure how this interacts with split lines 
+            # TODO CHECK AGAINST THE WEBSITE SOURCE
 
-            for row in rows:
-                ## CURRENTLY ONLY WORKS FOR FAMILIES WITHOUT SPLIT LINES 
-                line = self.buildLine(row)
+            # splitLine is the constant data between split evolution lines
+            splitLine = []
+            for row in table.findAll("tr"):
+                # if the row is not an evolutionary line, reset splitLine and skip it
+                if not isEvoLine(row):
+                    splitLine = []
+                    continue
+                                
+                line = self.buildLine(row, splitLine)
                 res[find(line, lambda node: node.is_root).name] = line
-                # print(find(line, lambda node: node.is_root).name)
-                line.hshow()
-                for x in res.keys(): res[x].hshow()
-                continue
-                # break
+                                
+                # if the row is an evolutionary line and splitLine has not been assigned, assign it
+                if splitLine == []:
+                    splitLine = row.findAll("td", rowspan=True)
                 
-            return res
+        return res
     
-    def buildLine(self, data):
-        ## THIS CURRENTLY ONLY WORKS FOR FAMILIES WITHOUT SPLIT LINES
+    def buildLine(self, data, splitLine):
         # each td is each cell in the row 
-        allCells = data.findAll("td")
+        dataCells = data.findAll("td")
+        allCells = splitLine + dataCells
+
         # keep only the cells that don't have images (every third cell contains an image)
         relevantCells = [allCells[i] for i in range(1, len(allCells)) if (i-1) % 3 == 0]
         # keep only the relevant text
         evoText = [cell.text.strip() for cell in relevantCells]
         # write the node
         line = list_to_tree(["/".join(evoText)])
-
-        # line.hshow()
 
         return line
 
