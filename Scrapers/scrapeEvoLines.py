669ab26515eda158f2fe6d3100de37d960254e9f
violll <gilbarthol3@gmail.com>
2024-05-03 10:42:54 -0400

    feat: getSoup method written

--- Scrapers/scrapeEvoLines.py
+++ Scrapers/scrapeEvoLines.py
@@ -1,7 +1,17 @@
-import bs4
+from bs4 import BeautifulSoup
+import requests
 import bigtree
 
 class scrapeEvoLines():
     def __init__(self) -> None:
         # The url from which the evo lines are scraped
         self.url = "https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_evolution_family"
+        self.soup = self.getSoup()
+
+    def getSoup(self):
+        html = requests.get(self.url)
+        return BeautifulSoup(html.text, 'html.parser')
+    
+
+if __name__ == "__main__":
+    scrapeEvoLines()
