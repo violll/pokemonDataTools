30b40fb8c7c80b86d46f45d1c8cc03f90c0be756
violll <gilbarthol3@gmail.com>
2024-05-11 14:59:46 -0400

    refactor: convert to class

--- ShowdownGenerator/Platinum/platinumShowdownGenerator.py
+++ ShowdownGenerator/Platinum/platinumShowdownGenerator.py
@@ -3,99 +3,105 @@ from urllib.request import Request, urlopen
 import pandas as pd
 import io
 
-f = open("ShowdownGenerator/Platinum/Full Pokemon Platinum Trainer Data.txt", "r")
-txt = f.read().split("\n\n")
+class PlatinumShowdown:
+    def __init__(self) -> None:
+        self.txt = self.readTrainerData()
+        self.formatTrainer()
 
-def formatTrainer(txt):
-    possibleTrainers = []
+    def readTrainerData(self):
+        f = open("ShowdownGenerator/Platinum/Full Pokemon Platinum Trainer Data.txt", "r")
+        return f.read().split("\n\n")
 
-    while possibleTrainers == []:
-        desiredTrainer = input("What is the trainer name?\n> ")
+    def formatTrainer(self):
+        possibleTrainers = []
+
+        while possibleTrainers == []:
+            desiredTrainer = input("What is the trainer name?\n> ")
+            
+            # search for multiple versions of the same trainer
+            for i in range(len(self.txt)):
+                if desiredTrainer in self.txt[i]: possibleTrainers.append(i)
+
+            if possibleTrainers == []: print("Please type a valid trainer name!")
+
+        # user selects the appropriate trainer
+        if len(possibleTrainers) > 1: 
+            for i in possibleTrainers: print(self.txt[i])
+            trainer = self.txt[int(input("Which version is correct?\n> "))].split("\n")[1:]
+        else: 
+            trainer = self.txt[possibleTrainers[0]].split("\n")[1:]
+
+        # checks if trainer has items and writes showdown import for each pokemon
+        res = ""
+        isMon = False
+        for line in trainer:
+            if "Item" in line: print("WARNING: {} has {}".format(desiredTrainer, line.replace("Items", "items")))
+            if isMon: res += self.formatPokemon(line, desiredTrainer)
+            if "=" in line: isMon = True
         
-        # search for multiple versions of the same trainer
-        for i in range(len(txt)):
-            if desiredTrainer in txt[i]: possibleTrainers.append(i)
+        print(res)
+        return res
+            
+    def formatPokemon(self, pokemon, trainer):
+        res = ""
+        attributes = pokemon.split(" ")
+        name = attributes[0]
+        level = attributes[2]
+        attributeDict = dict()
+        attributeStr = pokemon[pokemon.find("(")+1:-1].split(",")
+        for attr in attributeStr:
+            key, val = attr.split(": ")
+            attributeDict[key.strip()] = val.strip()
 
-        if possibleTrainers == []: print("Please type a valid trainer name!")
+        # if IVs are different for each stat
+        if "/" in attributeDict["IVs"]:
+            attributeDict["IVs"] = attributeDict["IVs"].split("/")
+            print(attributeDict["IVs"])
+        else:
+            attributeDict["IVs"] = [attributeDict["IVs"] for _ in range(6)]
 
-    # user selects the appropriate trainer
-    if len(possibleTrainers) > 1: 
-        for i in possibleTrainers: print(txt[i])
-        trainer = txt[int(input("Which version is correct?\n> "))].split("\n")[1:]
-    else: 
-        trainer = txt[possibleTrainers[0]].split("\n")[1:]
+        res += "{} ({}) ({})\n".format(trainer, name, input("What gender is {}'s {}?\n> ".format(trainer, name)))
+        res += "IVs: {} HP / {} Atk / {} Def / {} SpA / {} SpD / {} Spe\n".format(attributeDict["IVs"][0], attributeDict["IVs"][1], attributeDict["IVs"][2], attributeDict["IVs"][3], attributeDict["IVs"][4], attributeDict["IVs"][5])
+        res += "Ability: {}\n".format(attributeDict["Ability"])
+        res += "Level: {}\n".format(level)
+        res += "{} Nature\n".format(attributeDict["Nature"])
+        if attributeDict["Moves"] == "Default":
+            res += self.getMoves(name, level)
+        else: 
+            for move in attributeDict["Moves"].split("/"):
+                res += "- {}\n".format(move)
+        return res
 
-    # checks if trainer has items and writes showdown import for each pokemon
-    res = ""
-    isMon = False
-    for line in trainer:
-        if "Item" in line: print("WARNING: {} has {}".format(desiredTrainer, line.replace("Items", "items")))
-        if isMon: res += formatPokemon(line, desiredTrainer)
-        if "=" in line: isMon = True
-    
-    print(res)
-    return res
-        
-def formatPokemon(pokemon, trainer):
-    res = ""
-    attributes = pokemon.split(" ")
-    name = attributes[0]
-    level = attributes[2]
-    attributeDict = dict()
-    attributeStr = pokemon[pokemon.find("(")+1:-1].split(",")
-    for attr in attributeStr:
-        key, val = attr.split(": ")
-        attributeDict[key.strip()] = val.strip()
-
-    # if IVs are different for each stat
-    if "/" in attributeDict["IVs"]:
-        attributeDict["IVs"] = attributeDict["IVs"].split("/")
-        print(attributeDict["IVs"])
-    else:
-        attributeDict["IVs"] = [attributeDict["IVs"] for _ in range(6)]
-
-    res += "{} ({}) ({})\n".format(trainer, name, input("What gender is {}'s {}?\n> ".format(trainer, name)))
-    res += "IVs: {} HP / {} Atk / {} Def / {} SpA / {} SpD / {} Spe\n".format(attributeDict["IVs"][0], attributeDict["IVs"][1], attributeDict["IVs"][2], attributeDict["IVs"][3], attributeDict["IVs"][4], attributeDict["IVs"][5])
-    res += "Ability: {}\n".format(attributeDict["Ability"])
-    res += "Level: {}\n".format(level)
-    res += "{} Nature\n".format(attributeDict["Nature"])
-    if attributeDict["Moves"] == "Default":
-        res += getMoves(name, level)
-    else: 
-        for move in attributeDict["Moves"].split("/"):
-            res += "- {}\n".format(move)
-    return res
-
-def getMoves(name, level):
-    try:
-        df = pd.read_pickle("ShowdownGenerator/Platinum/PokemonData/{}.pkl".format(name))
-        col = list(df)[0]
-    except:
-        req = Request("https://bulbapedia.bulbagarden.net/wiki/{}_(Pok%C3%A9mon)/Generation_IV_learnset".format(name))
-        req.add_header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0")
-        html = urlopen(req).read().decode("utf-8")
-        soup = BeautifulSoup(html, "html.parser")
-
-        table = soup.find("table", {"class": "sortable"})
-
-        try: 
-            df = pd.read_html(io.StringIO(str(table)), displayed_only=False)[0].loc[:, ["PtHGSS", "Move"]]
-            col = "PtHGSS"
+    def getMoves(self, name, level):
+        try:
+            df = pd.read_pickle("ShowdownGenerator/Platinum/PokemonData/{}.pkl".format(name))
+            col = list(df)[0]
         except:
-            df = pd.read_html(io.StringIO(str(table)), displayed_only=False)[0].loc[:, ["Level", "Move"]]
-            col = "Level"
+            req = Request("https://bulbapedia.bulbagarden.net/wiki/{}_(Pok%C3%A9mon)/Generation_IV_learnset".format(name))
+            req.add_header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0")
+            html = urlopen(req).read().decode("utf-8")
+            soup = BeautifulSoup(html, "html.parser")
 
-        for i in range(df.shape[0]):
-            val = df.loc[i, col]
-            if val == "N/AN/A": 
-                df.loc[i, col] = -1
-            else: df.loc[i, col] = int(str(val)[:len(str(val))//2])
+            table = soup.find("table", {"class": "sortable"})
 
-        df = df[df.loc[:,col] != -1]
-        df.to_pickle("ShowdownGenerator/Platinum/PokemonData/{}.pkl".format(name))
+            try: 
+                df = pd.read_html(io.StringIO(str(table)), displayed_only=False)[0].loc[:, ["PtHGSS", "Move"]]
+                col = "PtHGSS"
+            except:
+                df = pd.read_html(io.StringIO(str(table)), displayed_only=False)[0].loc[:, ["Level", "Move"]]
+                col = "Level"
 
-    moves = "".join(list(df[df.loc[:,col] <= int(level)].loc[:, "Move"].tail(4).transform(lambda x: "- " + x + "\n")))
-    return moves
+            for i in range(df.shape[0]):
+                val = df.loc[i, col]
+                if val == "N/AN/A": 
+                    df.loc[i, col] = -1
+                else: df.loc[i, col] = int(str(val)[:len(str(val))//2])
+
+            df = df[df.loc[:,col] != -1]
+            df.to_pickle("ShowdownGenerator/Platinum/PokemonData/{}.pkl".format(name))
+
+        moves = "".join(list(df[df.loc[:,col] <= int(level)].loc[:, "Move"].tail(4).transform(lambda x: "- " + x + "\n")))
+        return moves
 
 if __name__ == "__main__":
-    formatTrainer(txt)
+    PlatinumShowdown()
