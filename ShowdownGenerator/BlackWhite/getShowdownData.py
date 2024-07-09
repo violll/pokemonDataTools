64676a0c336d0055860f136ef1134eae96bb5121
violll <gilbarthol3@gmail.com>
2024-07-09 16:33:18 -0400

    feat: added wrapper showdown class to allow for command line use of all tools using a parser

--- ShowdownGenerator/BlackWhite/getShowdownData.py
+++ ShowdownGenerator/BlackWhite/getShowdownData.py
@@ -14,7 +14,7 @@ def getShowdownData():
     if "Cheren" in trainerName or "Bianca" in trainerName:
         sheetName = input("What was your starter pokemon?\n> ")
 
-    df = pd.read_excel("ShowdownGenerator/Black&White/Pokémon Black Trainer info (Lillipup).xlsx", sheet_name=sheetName, index_col=list(range(0,5)))
+    df = pd.read_excel("ShowdownGenerator/BlackWhite/Pokémon Black Trainer info (Lillipup).xlsx", sheet_name=sheetName, index_col=list(range(0,5)))
     df = df.loc[:, :"IVs"]
 
     trainerDf = df.loc[df.index.get_level_values(1).str.contains(trainerName)]
