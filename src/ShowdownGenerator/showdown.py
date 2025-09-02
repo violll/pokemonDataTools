76ee3a380db7764e5f4de9f844ff9ea2f9c67889
violll <gilbarthol3@gmail.com>
2025-09-02 15:43:56 +0100

    refactor: modified implementation of argparser to allow for easy test writing

--- src/ShowdownGenerator/showdown.py
+++ src/ShowdownGenerator/showdown.py
@@ -12,28 +12,44 @@ import ORAS.getShowdownData as ORS
 import Platinum.getShowdownData as Pt
 import XY.getShowdownData as XY
 
+GAMEVERSIONS = {
+    "Pt": Pt.PlatinumShowdown,
+    "BW": BW.getShowdownData,
+    "ORAS": ORS.ORAShowdown,
+    "XY": XY.XYShowdown
+}
 
+def getGameVersion(gameVersion):
+    return gameVersion
 class ShowdownWrapper():
-    def __init__(self) -> None:
-        self.gameVersions = {
-            "Pt": Pt.PlatinumShowdown,
-            "BW": BW.getShowdownData,
-            "ORAS": ORS.ORAShowdown,
-            "XY": XY.XYShowdown
-        }
+    def __init__(self, mode = "normal", test_param = None) -> None:
+        # validate argument options
+        self.parse_args(mode, test_param)
+        
+        # access showdown data
+        GAMEVERSIONS[self.game_version]()
 
-        self.parser = self.initParser()
-        self.args = self.parser.parse_args()
+    def parse_args(self, mode, test_param):
+        if mode == "unittest":
+            if test_param is None:
+                raise ValueError("please provide arguments for testing")
 
-        self.gameVersions[self.args.gameVersion]()
+            self.game_version = test_param["game_version"]
+        
+        else:
+            parser = argparse.ArgumentParser()
+            parser.add_argument("-v",                              \
+                                "--game_version",                  \
+                                choices = GAMEVERSIONS.keys(),     \
+                                help = "the game you are playing", \
+                                required=True)
+            
+            args = parser.parse_args()
 
-    def initParser(self):
-        parser = argparse.ArgumentParser()
-        parser.add_argument("gameVersion",                          \
-                            choices = self.gameVersions.keys(),     \
-                            help = "the game you are playing")
+            self.game_version = args.game_version
 
-        return parser
+    def get_game_version(self):
+        return self.game_version
 
 if __name__ == "__main__":
     ShowdownWrapper()
